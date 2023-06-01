93b52711d854281e18b0bd8e3924269f1bd509f5
Manuel Murillo <mmpeira@gmail.com>
2023-06-01 16:36:25 +0200

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+
+const Form = () => {
+  return (
+    <div>
+      <h2>Formulario</h2>
+    </div>
+  )
+}
+
+export default Form
---
+++ src/components/PatientList.jsx
@@ -0,0 +1,10 @@
+
+const PatientList = () => {
+  return (
+    <div>
+      <h2>Listado pacientes</h2>
+    </div>
+  )
+}
+
+export default PatientList
